f2b09445a132dc253343e94752d956b82d8bdf8d
Guilherme Franco <glrmfranco@gmail.com>
2022-05-04 11:32:07 -0300

    ;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,83 @@
-const inputTarefa = document.querySelector('.input-tarefa')
-const btnTarefa = document.querySelector('.btn-tarefa')
-const tarefas = document.querySelector('.tarefas')
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
 
 btnTarefa.addEventListener('click', (e) => {
-    if (!inputTarefa.value) return
-    criaTarefa(inputTarefa.value)
-})
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
 
 function criaLi() {
-    const li = document.createElement('li')
-    return li
+    const li = document.createElement('li');
+    return li;
 }
 
 function criaTarefa(txtInput) {
-    const li = criaLi()
-    li.innerText = txtInput
-    tarefas.appendChild(li)
-    limpaInput()
-    criaBtnApagar(li)
-    salvarTarefas()
+    const li = criaLi();
+    li.innerText = txtInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBtnApagar(li);
+    salvarTarefas();
 }
 
 inputTarefa.addEventListener('keypress', (e) => {
-
     if (e.keyCode === 13) {
-        if (!inputTarefa.value) return
-        criaTarefa(inputTarefa.value)
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
     }
-})
+});
 
 function limpaInput() {
-    inputTarefa.value = ''
-    inputTarefa.focus()
+    inputTarefa.value = '';
+    inputTarefa.focus();
 }
 
 function criaBtnApagar(li) {
-    li.innerText += ' '
-    const botaoApagar = document.createElement('button')
-    botaoApagar.setAttribute('class', 'apagar')
-    botaoApagar.innerText = 'Apagar'
-    li.appendChild(botaoApagar)
-    salvarTarefas()
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.innerText = 'X';
+    li.appendChild(botaoApagar);
+    salvarTarefas();
 }
 
 function apagaTudo() {
-    const lista = tarefas.querySelectorAll('li')
-    lista.forEach(el => el.remove())
-
+    const lista = tarefas.querySelectorAll('li');
+    lista.forEach((el) => el.remove());
 }
 
 document.addEventListener('click', (e) => {
-    const et = e.target
+    const et = e.target;
     if (et.classList.contains('apagar')) {
-        et.parentElement.remove()
-        salvarTarefas()
+        et.parentElement.remove();
+        salvarTarefas();
     }
     if (et.classList.contains('apaga-tudo')) {
-        apagaTudo()
+        apagaTudo();
     }
-})
+});
 
 function salvarTarefas() {
-    const liTarefas = tarefas.querySelectorAll('li')
-    const listaDeTarefas = []
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
 
     for (let tarefa of liTarefas) {
-        let tarefaTexto = tarefa.innerText
-        tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
-        listaDeTarefas.push(tarefaTexto)
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('X', '').trim();
+        listaDeTarefas.push(tarefaTexto);
     }
-    const tarefasJSON = JSON.stringify(listaDeTarefas)
-    localStorage.setItem('tarefas', tarefasJSON)
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
 }
 
 function adicionaTarefasSalvas() {
-    const tarefas = localStorage.getItem('tarefas')
-    const listaDeTarefas = JSON.parse(tarefas)
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
 
     for (let tarefa of listaDeTarefas) {
-        criaTarefa(tarefa)
+        criaTarefa(tarefa);
     }
 }
 
-adicionaTarefasSalvas()
+adicionaTarefasSalvas();
